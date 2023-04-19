51039f41377384d8dcf8adfcb91f8183f8bec6a2
Anish Patil <101693650+anish-patil@users.noreply.github.com>
2023-04-20 00:22:15 +0530

    Add files via upload

---
+++ Drum Kit/index.js
@@ -0,0 +1,62 @@
+var btn
+//button press
+for(let i=0;i<7;i++)    
+{
+    document.querySelectorAll("button")[i].addEventListener("click",function()
+    {
+       var btn_pressed=this.innerHTML
+        makeSound(btn_pressed)
+    }) 
+}
+
+// Detect keyboard press
+document.addEventListener("keydown",function(event){
+    makeSound(event.key)
+    console.log(event)
+    
+})
+
+function makeSound(key){
+    switch (key) {
+
+        case "w":
+            tom1= new Audio("./sounds/tom-1.mp3");
+            tom1.play();
+        break;
+
+        case "a":
+            tom2= new Audio("./sounds/tom-2.mp3");
+            tom2.play();
+        break;
+        
+        case "s":
+            tom3= new Audio("./sounds/tom-3.mp3");
+            tom3.play();
+        break;
+
+        case "d":
+            tom4= new Audio("./sounds/tom-4.mp3");
+            tom4.play();
+        break;
+
+        case "j":
+            crash= new Audio("./sounds/crash.mp3");
+            crash.play();
+        break;
+
+        case "k":
+        kick= new Audio("./sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+        case "l":
+        snare= new Audio("./sounds/snare.mp3");
+        snare.play();
+        break;
+       
+        default:
+        console.log(this.innerHTML)
+        break;
+
+    }
+}
